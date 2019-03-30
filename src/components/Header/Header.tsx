4a297e846bfce1384a0e3dcef0d20c4c75c763c5
Sander Brugge <sander.brugge@moovly.com>
2019-03-30 14:14:54 +0100

    header bar is in place

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
-import * as React from 'react';
-import './Header.scss';
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Header.scss";
 
 const Header: React.FC = () => {
   return (
-    <div className="header_container">Header</div>
+    <div className="header_container">
+      <span className="navigation_title">Kanban</span>
+      <FontAwesomeIcon icon={faUserCircle} className="user_icon" />
+    </div>
   );
-}
+};
 
 export default Header;
