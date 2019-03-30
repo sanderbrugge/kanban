4347e7b244ee06ae323f140555df755ebf127f50
Sander Brugge <sander.brugge@moovly.com>
2019-03-30 16:47:23 +0100

    added a cardview + ui

--- src/components/SwimmingLane/SwimmingLaneRow.tsx
+++ src/components/SwimmingLane/SwimmingLaneRow.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { User } from "../../api/interfaces";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./SwimmingLane.scss";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 interface IProps {
   user: User;
@@ -7,10 +10,13 @@ interface IProps {
 
 const SwimmingLaneRow: React.FC<IProps> = ({ user }) => {
   return (
-    <>
-      <div>{user.name}</div>
-      <div>{user.age}</div>
-    </>
+    <div className="swimminglane-row-container">
+      <img src={user.thumbnail} className="thumbnail" />
+      <div className="info">
+        <span>{user.name}<span className="rating"><FontAwesomeIcon icon={faStar}/> {user.rating}</span></span>
+        <span>{`${user.age} y/o - ${user.sex} - ${user.location}`}</span>
+      </div>
+    </div>
   );
 };
 
