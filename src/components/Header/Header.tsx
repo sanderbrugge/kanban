54a2ed4cf4432342ad03caa8cead7064cf9e537d
Sander Brugge <sander.brugge@moovly.com>
2019-03-30 13:54:22 +0100

    add sass for variables, init folder structure + basic nav header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './Header.scss';
+
+const Header: React.FC = () => {
+  return (
+    <div className="header_container">Header</div>
+  );
+}
+
+export default Header;
---
+++ src/scenes/Home/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Header from '../../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <Header />
+  );
+}
+
+export default Home;
