13b7ecca2bc9a0483041cef7ffd2ae308bdd9378
Sander Brugge <sander.brugge@moovly.com>
2019-03-31 13:30:35 +0200

    added react dnd deps, made the cards draggable

--- src/components/SwimmingLane/SwimmingLaneRow.tsx
+++ src/components/SwimmingLane/SwimmingLaneRow.tsx
@@ -3,13 +3,38 @@ import { User } from "../../api/interfaces";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./SwimmingLane.scss";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { DragSource, ConnectDragSource } from "react-dnd";
 
 interface IProps {
+  connectDragSource: ConnectDragSource;
   user: User;
 }
 
-const SwimmingLaneRow: React.FC<IProps> = ({ user }) => {
-  return (
+const itemSource =  {
+  beginDrag(props: any) {
+    return props.item;
+  },
+  endDrag(props: any, monitor: any, component: any) {
+    if(!monitor.didDrop()) {
+      return;
+    }
+
+    return props.handleDrop(props.item.id);
+  }
+};
+
+function collect(connect: any, monitor: any) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+
+const SwimmingLaneRow: React.FC<IProps> = ({ user, connectDragSource }) => {
+
+  return connectDragSource(
     <div className="swimminglane-row-container">
       <img src={user.thumbnail} className="thumbnail" />
       <div className="info">
@@ -20,4 +45,6 @@ const SwimmingLaneRow: React.FC<IProps> = ({ user }) => {
   );
 };
 
-export default SwimmingLaneRow;
+
+export default DragSource('row', itemSource, collect)(SwimmingLaneRow);
+
